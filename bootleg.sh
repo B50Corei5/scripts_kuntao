655bafcd1532af56784701311bc893a852b26166
B50Corei5 <36875492+B50Corei5@users.noreply.github.com>
2018-07-01 14:39:07 +1200

    Update bootleg.sh

--- bootleg.sh
+++ bootleg.sh
@@ -42,10 +42,3 @@ echo -e ""
 echo -e "${CLR_BLD_GRN}Deleting Doze to avoid build errors${CLR_RST}"
 rm -rf device/lenovo/kuntao/doze
 echo -e ""
-echo -e "${CLR_BLD_GRN}Compilation starting${CLR_RST}"
-source build/envsetup.sh
-lunch bootleg_kuntao-userdebug
-brunch bootleg_kuntao-userdebug -j8
-eco -e ""
-echo -e "${CLR_BLD_GRN}Closing the server - Task Complete${CLR_RST}"
-sudo shutdown -h now
