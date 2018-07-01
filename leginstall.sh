bdcb1136ac09fa4b78ab48e0aa767a5090aa517b
B50Corei5 <36875492+B50Corei5@users.noreply.github.com>
2018-07-01 14:39:40 +1200

    Update leginstall.sh

--- leginstall.sh
+++ leginstall.sh
@@ -1,6 +1,7 @@
-make installclean
+echo -e "${CLR_BLD_GRN}Compilation starting${CLR_RST}"
 source build/envsetup.sh
-lunch 
-5
-brunch bootleg_kuntao-userdebug
+lunch bootleg_kuntao-userdebug
+brunch bootleg_kuntao-userdebug -j8
+eco -e ""
+echo -e "${CLR_BLD_GRN}Closing the server - Task Complete${CLR_RST}"
 sudo shutdown -h now
